c090136ab926a8ff59c6fce961de2b6a17414374
Diaspar <the.diaspar@gmail.com>
2018-07-31 17:34:35 -0400

    game fixes

---
+++ assets/javascript/game.js
@@ -0,0 +1,63 @@
+/* =================================================
+        Crystal Collector Pseudo Code: 
+====================================================*/ 
+//set up game 
+//write pseudo code here
+
+
+
+
+/*==================================================
+              Global Variables
+===================================================*/
+
+var wins = 0;
+var losses = 0;
+
+var target = 0; 
+var score = 0;
+
+
+var crystal1 = Math.floor(Math.random()*10);
+var crystal2 = Math.floor(Math.random()*10);
+var crystal3 = Math.floor(Math.random()*10);
+var crystal4 = Math.floor(Math.random()*10);
+
+var randomNumber = Math.floor(Math.random()*100);
+  
+
+
+
+
+
+/*==================================================
+                      Functions
+====================================================*/
+
+function startAndResetGame (){
+
+}
+
+
+/*==================================================
+                      Game
+===================================================*/
+
+ //crystal click functions
+
+$("#blue").on ('click', function(){
+   
+  }); 
+
+  $("#purple").on ('click', function(){
+
+  });
+
+  $("#green").on ('click', function(){
+ 
+  }); 
+  
+  $("#red").on ('click', function(){
+  
+  });   
+
