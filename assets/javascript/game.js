1020cab9b9f2a6238b1d7f6fc7cff8c48856cc34
Diaspar <the.diaspar@gmail.com>
2018-08-06 02:42:47 -0400

    js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,62 +1,139 @@
+$( document ).ready(function(){
 
-/*==================================================
-              Global Variables
-===================================================*/
+  /* ============================================== */
+ /*    Establish variables and Dom maniplation     */
 
-var wins = 0;
-var losses = 0;
-
-var target = 0; 
-var score = 0;
-
-//create for loop for this later dry code
-
-var crystal1 = Math.floor(Math.random()*12) +1;
-var crystal2 = Math.floor(Math.random()*12) +1;
-var crystal3 = Math.floor(Math.random()*12) +1;
-var crystal4 = Math.floor(Math.random()*12) +1;
-
-//or create an object then loop through it?
-
-var randomNumber = Math.floor(Math.random()*120 +19);
-  
-
-
-
-
-
-/*==================================================
-                      Functions
-====================================================*/
-
-function startAndResetGame (){
-
-        var score = 0;
-
-        target = Math.floor(Math.random()*120)+19;
-
-}
+   //1:  Generate a random Target Number for user to guess 
+   var targetNumber = Math.floor(Math.random()*102)+19;
+        
+   //2: Display the target number in browser
+   $('#usertarget').text(targetNumber);
+        
+   //3: Generate each random crystal values 
+   var crystal1 = Math.floor(Math.random()*12)+1;
+   var crystal2 = Math.floor(Math.random()*12)+1;
+   var crystal3 = Math.floor(Math.random()*12)+1;
+   var crystal4 = Math.floor(Math.random()*12)+1;
+        
+   //4: Game counters 
+    var userScore = 0; 
+    var wins = 0;
+    var losses = 0;
+        
+    //5: display  wins and losses to user
+     $('#wincounter').text(wins);
+     $('#losscounter').text(losses);
 
 
-/*==================================================
-     Game Interactivity - DOM jQuery
-===================================================*/
 
- //crystal click 
 
-$("#blue").on ('click', function(){
+ /*===================================================*/
+ /*      Game Functions + Dom Manipulation            */
+      
+
+  // 1: Function -- Start and Reset the Game
+    
+  function startAndResetGame (){
+
+      targetNumber=Math.floor(Math.random()*102)+19;
+        $("#userTarget").text(targetNumber);
+
+      crystal1= Math.floor(Math.random()*12)+1;
+      crystal2 = Math.floor(Math.random()*12)+1;
+      crystal3= Math.floor(Math.random()*12)+1;
+      crystal4= Math.floor(Math.random()*12)+1;
+
+       userScore= 0;
+       $('#userScore').text(userScore);
+
+    } //function SRG ends
+
+ //2: Call SRG function
+
+    startAndResetGame();
+
+// 3: Crystal CLicks and Corresponding Functions
+
+    //Blue Crystal
+    $("#blue").on ("click", function(){
+     userScore = userScore + crystal1;
+          
+    $('#userscore').text(userScore); 
+                
+     if (userScore == targetNumber){
+                userwins();
+         } else if ( userScore > targetNumber){
+                userlosses();
+              }   
+
+     console.log(userScore);
+  })  
+
+     //Purple Crystal
+      $("#purple").on ("click", function(){
+        userScore =  userScore + crystal2;
+             
+       $('#userscore').text(userScore); 
+                   
+        if (userScore == targetNumber){
+                   userwins();
+            } else if ( userScore > targetNumber){
+                   userlosses();
+                 }   
+ })  
+
+
+     //Green Crystal
+     $("#green").on ("click", function(){
+        userScore = userScore+ crystal3;
+             
+       $('#userscore').text(userScore); 
+                   
+        if (userScore == targetNumber){
+                   userwins();
+            } else if ( userScore > targetNumber){
+                   userlosses();
+                 }   
+ })  
+
    
-  }); 
+     //Red Crystal
+     $("#red").on ("click", function(){
+        userScore = userScore + crystal4;
+             
+       $('#userscore').text(userScore); 
+                   
+        if (userScore == targetNumber){
+                   userwins();
+            } else if ( userScore > targetNumber){
+                   userlosses();
+                 }   
+ })  
 
-  $("#purple").on ('click', function(){
 
-  });
+  // 4:  Win and lose functions
+   
+    //If the user wins
+      
+       function userwins(){
+        swal("You win!");
+          wins++; 
 
-  $("#green").on ('click', function(){
- 
-  }); 
-  
-  $("#red").on ('click', function(){
-  
-  });   
+          $('#wincounter').text(wins);
+          startAndResetGame();
+        }
 
+    // If the user losses
+
+        function userlosses(){
+        swal ("You lose!");
+          losses++;
+
+          $('#losscounter').text(losses);
+          startAndResetGame()
+        }
+
+
+
+
+      }); 
